5a8446dd2d0f103d53495dd722ea5d8129d1a668
David Clark <dlc.clark@gmail.com>
2018-01-16 03:30:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function theBeatlesPlay(musicians, instruments){
+  var arr = [];
+  for (var i = 0; i < musicians.length; i++){
+    arr.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return arr;
+}
+
+function johnLennonFacts(arr){
+  var facts = [];
+  var i = 0;
+  while (i < arr.length);{
+    facts.push(arr[i] + "!!!");
+    i++;
+  }
+  return facts;
+}
